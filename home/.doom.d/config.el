23272fb63ae29f8c0b02be7113292f5839e4f8ac
Daksh Miglani <hello@dak.sh>
2020-09-05 23:09:24 +0530

    add doom config

---
+++ home/.doom.d/config.el
@@ -0,0 +1,79 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Daksh Miglani"
+      user-mail-address "d@dak.sh")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+(setq doom-font (font-spec :family "Operator Mono Lig Book" :size 19)
+      doom-big-font (font-spec :family "Operator Mono Lig Book" :size 25)
+      doom-variable-pitch-font (font-spec 
+                                 :family "Operator Mono Lig Book" :size 19)
+      doom-serif-font (font-spec :family "IBM Plex Mono" :weight 'light))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; relative line numbers
+(setq display-line-numbers-type 'relative)
+
+;; flyspell lazy
+(after! flyspell (require 'flyspell-lazy) (flyspell-lazy-mode 1))
+
+;; company config
+
+(after! company
+  (setq company-idle-delay 0
+        company-minimum-prefix-length 2)
+  (setq company-show-numbers t)
+(add-hook 'evil-normal-state-entry-hook #'company-abort)) 
+
+(setq-default history-length 1000)
+(setq-default prescient-history-length 1000)
+
+;; vue.js
+(add-hook 'vue-mode-hook #'lsp!)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
